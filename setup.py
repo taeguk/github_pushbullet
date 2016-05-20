41d653ae5b13d9734df907423a1f83aad78fa16e
taeguk <xornrbboy@gmail.com>
2016-05-20 05:02:43 -0400

    Restrict installation. Now users can install only using pip3.

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import sys
+
+if sys.version_info.major < 3:
+    msg = "Sorry, Python 2 is not supported (yet)"
+    print >> sys.stderr, msg
+    sys.exit(1)
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup (
     name = 'github_pushbullet',
-    version = '0.1.0',
+    version = '0.1.1',
 
     author = 'taeguk',
     author_email = 'xornrbboy@gmail.com',
@@ -26,5 +32,9 @@ setup (
 
     install_requires = ['github_listener', 'pushbullet.py'],
     
+    classifiers = [
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
     keywords = 'github notification push pushbullet',
 )
